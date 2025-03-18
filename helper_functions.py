ee42b864d384c0022d0b3492c0b448393b5fc3b7
Arthur Rusike <arthur.rusike@gmail.com>
2025-03-18 15:19:23 +1100

    Add New Zealand

--- helper_functions.py
+++ helper_functions.py
@@ -101,21 +101,29 @@ def load_specific_xls_sheet( file, sheet_name, header, use_cols):
      return cached_xls_sheet
 
 
+
 @st.cache_data
 def run_sql_query(startDate , endDate  ):
     """
     Connects to a SQL database using pyodbc
     """
     print(pyodbc.drivers())
+    try:
+        # conn_str = 'TRUSTED_CONNECTION=Yes; DSN=CalumoCoreDW; CONNECTION=ODBC; DATABASE=CoreDW; DRIVER={SQL Server Native Client 11.0}; DATASOURCE=scscl1dw01 '
+        conn_str = pyodbc.connect(
+            "DSN=CalumoCoreDW;"
+            "Trusted_Connection=yes;"
+        )
 
-    # conn_str = 'Trusted_Connection=Yes; DSN=CalumoCoreDW; DATABASE=CoreDW; '
-    conn_str = 'TRUSTED_CONNECTION=Yes; DSN=CalumoCoreDW; CONNECTION=ODBC; DATABASE=CoreDW; DRIVER={SQL Server Native Client 11.0}; DATASOURCE=scscl1dw01 '
-    slqQuery = f"SELECT * from CoreDW.[stgAQT].[vwRates] where [InvoiceDate] between '{str(startDate)}' AND '{str(endDate)}' and [Cost_Center] like '%S&H%' Order By SourceSystem, InvoiceNumber"
-    conn = pyodbc.connect(conn_str)
-    invoice_rates = pd.read_sql_query(slqQuery, conn)
-    conn.close()
+        slqQuery = f"SELECT * from CoreDW.[stgAQT].[vwRates] where [InvoiceDate] between '{str(startDate)}' AND '{str(endDate)}' and [Cost_Center] like '%S&H%' Order By SourceSystem, InvoiceNumber"
+        invoice_rates = pd.read_sql_query(slqQuery, conn_str)
+        conn_str.close()
+
+        return invoice_rates
+
+    except pyodbc.Error as e:
+        print("Error in connection:", e)
+        return None
 
 
-
-    return invoice_rates
 
